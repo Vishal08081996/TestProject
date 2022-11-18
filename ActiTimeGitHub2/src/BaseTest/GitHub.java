6663c48d1bcc98c73e28df1cf8f57d9c26cf5f3b
Vishal08081996 <118370017+Vishal08081996@users.noreply.github.com>
2022-11-18 17:17:01 +0530

    Update GitHub.java

--- ActiTimeGitHub2/src/BaseTest/GitHub.java
+++ ActiTimeGitHub2/src/BaseTest/GitHub.java
@@ -3,7 +3,7 @@ package BaseTest;
 public class GitHub {
 
 	public static void main(String[] args) {
-		System.out.println("hello word");
+		System.out.println("hello word i am your owner ");
 
 	}
 
